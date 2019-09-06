97fdce6aac04e88ac5922869416776f3012531a9
xuanren.long <xuanren.long@mythware.com>
2019-09-06 14:08:58 +0800

    Support command line pass parameters

--- sqlite2json/sqlite2json.py
+++ sqlite2json/sqlite2json.py
@@ -1,8 +1,10 @@
 # coding=utf-8
 
+import getopt
 import json
 import os
 import sqlite3
+import sys
 
 
 def end_width(*suffix):
@@ -24,14 +26,22 @@ def remove_keys(d, *args):
         if d.__contains__(key):
             del d[key]
 
-def export_database_data(f_dir, f_name):
+def usage_help():
+    print('This is a program that exports sqlite tables to json files')
+    print('-i \tinput database file')
+    print('-o \toutput dir')
+    print('-h \tshow help')
+
+def export_database_data(i_path, o_dir):
     # create export dir
-    export_dir = os.path.join(os.path.join(f_dir, 'export'), os.path.splitext(f_name)[0])
-    if not os.path.exists(export_dir):
-        os.makedirs(export_dir)
+    if not os.path.exists(o_dir):
+        os.makedirs(o_dir)
+        
+    print('input file path: ' + i_path)
+    print('output dir path: ' + o_dir)
 
     # connect to the SQlite databases
-    connection = sqlite3.connect(os.path.join(f_dir, f_name))
+    connection = sqlite3.connect(i_path)
     connection.row_factory = dict_factory
 
     cursor = connection.cursor()
@@ -42,14 +52,14 @@ def export_database_data(f_dir, f_name):
     # for each of the bables , select all the records from the table
     for table_name in tables:
         # table_name = table_name[0]
-        print(table_name['name'])
+        print('export table name: ' + table_name['name'])
 
         cursor.execute('SELECT * FROM "%s"' % (table_name['name']))
 
         # fetch all or one we'll go for all.
         table_data = cursor.fetchall()
 
-        print(table_data)
+        # print(table_data)
 
         # delete useless key
         useless_key = ('delete_flag', 'restart_time', 'sku', 'summary', 'suspend_time')
@@ -58,7 +68,7 @@ def export_database_data(f_dir, f_name):
             remove_keys(dic, *useless_key)
 
         # generate and save JSON files with the table name for each of the database tables
-        with open(os.path.join(export_dir, table_name['name'] + '.json'), 'w') as the_file:
+        with open(os.path.join(o_dir, table_name['name'] + '.json'), 'w') as the_file:
             the_file.write(json.dumps(table_data, sort_keys=True, indent=4, separators=(',', ': ')))
 
     cursor.close()
@@ -66,13 +76,36 @@ def export_database_data(f_dir, f_name):
 
 
 if __name__ == '__main__':
-    root_dir = os.path.dirname(os.path.abspath(__file__))
-    list_file = os.listdir(root_dir)
+    input_file =''
+    output_dir = ''
+    opts, args = getopt.getopt(sys.argv[1:], "hi:o:")
 
-    # search database file
-    f_func = end_width('.db')
-    f_file = filter(f_func, list_file)
+    for op, value in opts:
+        if op == "-i":
+            input_file = value
+        elif op == "-o":
+            output_dir = value
+        elif op == "-h":
+            usage_help()
+            sys.exit()
 
-    # export each database file
-    for i in f_file:
-        export_database_data(root_dir, i)
+    if output_dir == '':
+        output_dir = os.path.dirname(os.path.abspath(__file__))
+
+    if input_file == '':
+        list_file = os.listdir(output_dir)
+
+        # search database file
+        f_func = end_width('.db')
+        f_file = filter(f_func, list_file)
+
+        # export each database file
+        for i in f_file:
+            export_database_data(os.path.join(output_dir, i), os.path.join(output_dir, os.path.splitext(i)[0]))
+
+    elif os.path.exists(input_file):
+        dirname, filename = os.path.split(input_file)
+        export_database_data(input_file, os.path.join(output_dir, filename.split('.',1)[0]))
+
+    else:
+        print('input file is not exists')
