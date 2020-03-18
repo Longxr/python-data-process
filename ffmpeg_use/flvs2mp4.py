58f095dbb67220795dbb42561b1ba70791d163bd
Longxr <longxuanren@gmail.com>
2020-03-18 17:29:41 +0800

    打印运行时间

--- ffmpeg_use/flvs2mp4.py
+++ ffmpeg_use/flvs2mp4.py
@@ -1,26 +1,20 @@
-#coding=utf-8
+# coding=utf-8
 import logging
 import os
 import shutil
 import sys
+from time import perf_counter
 
 from ffmpy import FFmpeg
 from natsort import natsorted
 
 logger = logging.getLogger(__name__)
 
+
 def video_convert(in_path, out_path):
-    FFmpeg(inputs={in_path: None}, outputs={out_path: '-loglevel quiet -c copy -bsf:v h264_mp4toannexb -f mpegts'}).run()
+    FFmpeg(inputs={in_path: None}, outputs={
+           out_path: '-loglevel quiet -c copy -bsf:v h264_mp4toannexb -f mpegts'}).run()
 
-
-def execCmd(cmd):
-    """
-    执行计算命令时间
-    """
-    r = os.popen(cmd)
-    text = r.read().strip()
-    r.close()
-    return text
 
 def del_path(path):
     if not os.path.exists(path):
@@ -52,22 +46,23 @@ def flv_dir_to_mp4(in_path_dir, out_path_file):
 
     # 生成中间文件ts
     for root, dirs, files in os.walk(in_path_dir):
-            files = natsorted(files)
-            for file in files:
-                if os.path.splitext(file)[1] == '.flv':
-                    file_path = os.path.join(root, file)
-                    video_convert(os.path.join(root, file), os.path.join(tsfile_dir, os.path.splitext(file)[0] + '.ts'))
+        files = natsorted(files)
+        for file in files:
+            if os.path.splitext(file)[1] == '.flv':
+                file_path = os.path.join(root, file)
+                video_convert(os.path.join(root, file), os.path.join(
+                    tsfile_dir, os.path.splitext(file)[0] + '.ts'))
 
     # 获取列表参数
     for root, dirs, files in os.walk(tsfile_dir):
-            files = natsorted(files)
-            for file in files:
-                if os.path.splitext(file)[1] == '.ts':
-                    file_path = os.path.join(root, file)
-                    out_files.append(file_path)
+        files = natsorted(files)
+        for file in files:
+            if os.path.splitext(file)[1] == '.ts':
+                file_path = os.path.join(root, file)
+                out_files.append(file_path)
 
-    ff = FFmpeg(inputs={'concat:' + '|'.join(out_files): None}, 
-                        outputs={out_path_file: '-loglevel quiet -c copy -absf aac_adtstoasc -movflags faststart'})
+    ff = FFmpeg(inputs={'concat:' + '|'.join(out_files): None},
+                outputs={out_path_file: '-loglevel quiet -c copy -absf aac_adtstoasc -movflags faststart'})
     ff.run()
     # cmd = '|'.join(out_files)
     # print(out_files)
@@ -75,10 +70,11 @@ def flv_dir_to_mp4(in_path_dir, out_path_file):
     # cmd = 'ffmpeg -i ' + cmd + ' -loglevel quiet -c copy -absf aac_adtstoasc -movflags faststart ' + out_path_file
     # cmd = 'ffmpeg -i ' + cmd + ' -safe 0 -segment_time_metadata 1 -vf select=concatdec_select -af aselect=concatdec_select,aresample=async=1 ' + out_path_file
     # print(cmd)
-    # print(execCmd(cmd))
 
 
 if __name__ == '__main__':
+    start = perf_counter()
+
     print('sys argv', sys.argv)
     if len(sys.argv) == 3:
         flv_dir_to_mp4(sys.argv[1], sys.argv[2])
@@ -87,3 +83,6 @@ if __name__ == '__main__':
     else:
         root_dir = os.path.dirname(os.path.abspath(__file__))
         flv_dir_to_mp4(root_dir, os.path.join(root_dir, 'out.mp4'))
+
+    end = perf_counter()
+    print('Running time: %s Seconds' % (end-start))
